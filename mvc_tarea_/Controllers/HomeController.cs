b936f34f227fcc83542c8886a86ddc05aaf920d4
jeremy benavidez <jeremybena99@gmail.com>
2021-11-09 22:28:54 -0600

    Agregar archivos de proyecto.

---
+++ mvc_tarea_/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_tarea_.Models;
+namespace mvc_tarea_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var Personal = new personal()
+            {
+
+                nombre = "Jeremy", apellido = "Benavides", edad = 21, correo = "jeremybena99@gmail.com", direccion = "san miguel", Telefono = "5555-5555"
+            };
+
+            return View(Personal);
+        }
+
+    }
+}
---
+++ mvc_tarea_/Models/PersonalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace mvc_tarea_.Models
+{
+  
+        public class personal
+        {
+            public string nombre { get; set; }
+            public string apellido { get; set; }
+            public int edad { get; set; }
+            public string correo { get; set; }
+            public string direccion { get; set; }
+            public string Telefono { get; set; }
+        }
+    }
